2b3d4dfe1317b9784203d863f237817060ee8c44
yiw036 <50216905+yiw036@users.noreply.github.com>
2021-02-14 23:47:15 -0800

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -2,4 +2,21 @@
 
 window.addEventListener('DOMContentLoaded', () => {
   // TODO
+
+  var item = JSON.parse(localStorage.getItem('products'));
+  var productList = document.getElementById('product-list');
+
+  if (localStorage.getItem('products') == null){
+    fetch('https://fakestoreapi.com/products')
+    .then(response => response.json())
+    .then(data => {
+      localStorage.setItem('products', JSON.stringify(data));
+    });
+
+  } else {    
+    for(let i = 0; i < item.length; i++){
+      productList.appendChild(new ProductItem(item[i]));
+    }
+  }
+  
 });
